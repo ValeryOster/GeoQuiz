73003227dc37de1ad02217d37c4e04c3d49eb1be
Valery Oster <oster_valera@hotmail.com>
2015-05-04 17:06:48 +0200

    First Version

---
+++ app/src/main/java/de/valeryoster/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package de.valeryoster.geoquiz;
+
+/**
+ * Created by Aleksejew on 23.04.15.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion)
+    {
+        mQuestion=question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
